a8d03c33f4fda9cc67de1bb13060392d596734e6
Stephen Fraser <stephen.fraser@digirati.com>
2023-01-17 13:29:22 +0000

    Skeleton repository

---
+++ scripts/build.mjs
@@ -0,0 +1,41 @@
+import { defineConfig } from './base-config.mjs';
+import { build } from 'vite';
+import chalk from 'chalk';
+import { execa } from "execa";
+
+(async () => {
+
+  const DIST = 'dist';
+
+  // Main UMD build.
+  buildMsg('UMD');
+  await build(
+    defineConfig({
+      entry: `src/index.umd.ts`,
+      name: 'index',
+      outDir: DIST,
+      globalName: 'IIIFThumbnailPanel',
+    })
+  );
+
+  buildMsg('@iiif/thumbnail-panel');
+  await build(
+    defineConfig({
+      entry: `src/index.ts`,
+      name: 'index',
+      outDir: `${DIST}/bundle`,
+    })
+  );
+
+  buildMsg('Types');
+
+  listItem('@iiif/thumbnail-panel');
+  await execa('./node_modules/.bin/dts-bundle-generator', [`--out-file=${DIST}/index.d.ts`, './src/index.ts']);
+
+  function buildMsg(name) {
+    console.log(chalk.grey(`\n\nBuilding ${chalk.blue(name)}\n`));
+  }
+  function listItem(name) {
+    console.log(chalk.gray(`- ${chalk.green(name)}`));
+  }
+})();
---
+++ src/__tests__/example.test.ts
@@ -0,0 +1,8 @@
+describe('test', function () {
+
+  test('example', () => {
+
+    expect(true).to.eq(true);
+
+  })
+});
