2b3e31d4c60f3b6e90077267c0b5f90426a2ca83
Adam J. Arling <aarling@gmail.com>
2023-02-14 14:14:12 -0600

    Testing some initial config and app structure
    Co-authored-by: Mat Jordan <mat@northwestern.edu>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { SpecificationBehaviors, ViewingDirection } from '@iiif/presentation-3';
+import { Collection, Manifest, SpecificationBehaviors, ViewingDirection } from '@iiif/presentation-3';
 import { convertPresentation2 } from '@iiif/parser/presentation-2';
 import { fetchManifest } from './lib/fetch-manifest';
+import { getThumbnails } from './lib/get-thumbnails';
+import { writeToTheDom } from './lib/write-to-dom';
 
 interface Config {
   iiif?: {
@@ -13,25 +15,30 @@ interface Config {
 const bookFixture = 'https://iiif.io/api/cookbook/recipe/0009-book-1/manifest.json';
 
 const defaultIIIFConfig = {
-  behavior: '',
-  viewingDirection: '',
+  behavior: ['paged'],
+  viewingDirection: 'left-to-right',
 };
 
-async function init(manifestId: string, config?: Config) {
-  if (!manifestId) return;
+async function init(resourceId: string, config?: Config) {
+  if (!resourceId) return;
 
   // Get manifest and convert to Pres 3
-  const manifestResponse = await fetchManifest(manifestId);
-  const pres3 = convertPresentation2(manifestResponse);
+  const resourceResponse = await fetchManifest(resourceId);
+  const pres3: Manifest | Collection = convertPresentation2(resourceResponse);
   console.log('pres3', pres3);
 
   // Grab destructured info from top level (or at Canvas level) of the manifest
-  const behavior = pres3['behavior'] || ['paged'];
-  const viewingDirection = pres3['viewingDirection'] || 'left-to-right';
+  const behavior = pres3['behavior'] || defaultIIIFConfig.behavior;
+  const viewingDirection = pres3['viewingDirection'] || defaultIIIFConfig.viewingDirection;
+
+  // Get thumbnails from helper
+  const thumbnails = getThumbnails(pres3.items);
+  console.log('thumbnails', thumbnails);
+
+  // Write to the DOM
+  thumbnails.forEach((thumb) => writeToTheDom(thumb));
 }
 
 init(bookFixture);
-
-// Create element here, render to the DOM
 
 export const test = 'thumbnail panel';
