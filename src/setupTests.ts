81a08b391f7111175d53d147964a76d1525ce827
Adam J. Arling <aarling@gmail.com>
2023-03-28 14:32:43 -0500

    Mock fetch for unit tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,7 +3,10 @@ import '@testing-library/jest-dom';
 import { afterEach, expect } from 'vitest';
 
 import { cleanup } from '@testing-library/react';
+import { fetch } from 'cross-fetch';
 import matchers from '@testing-library/jest-dom/matchers';
+
+global.fetch = fetch;
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
